cd420595f820c8adba8cc0ad6b4306e94e1a3be2
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-09-19 21:14:33 +0530

    Added initial routes and controllers

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-  res.send("Login route");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 const app = express();
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
@@ -13,8 +14,9 @@ app.get("/", (req, res) => {
 });
 
 //Using middlewares for authentication
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("App is running on port 8000");
+  connectToMongoDB();
+  console.log("App is running on port 5000");
 });

d607c0584d726366982d1cac24b403fcb8a6fbf3
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-09-19 20:28:40 +0530

    created server configurations

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+
 const app = express();
 
-app.listen(5000, () => {
-  console.log("App is running on port 5000");
+import authRoutes from "./routes/auth.routes.js";
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  //root route http://localhost:5000/
+  res.send("App is ready");
+});
+
+//Using middlewares for authentication
+app.use("api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("App is running on port 8000");
 });

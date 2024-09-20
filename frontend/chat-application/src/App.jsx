0cf74099b628e993a1189b8006dd7be12aa224ca
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-09-20 16:10:51 +0530

    Added Pages - inital

--- frontend/chat-application/src/App.jsx
+++ frontend/chat-application/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
-  return <></>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Login />
+    </div>
+  );
 }
 
 export default App;

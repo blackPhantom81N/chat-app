b86b0c09df8d929375210e2a7dd10aabc8f3799c
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-09-20 12:09:02 +0530

    Added sendMessage endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 const app = express();
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -12,10 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON payloads (from req.body)
 app.use(express.json());
-
+app.use(cookieParser());
 // Using middlewares for authentication routes
 app.use("/api/auth", authRoutes);
 
+app.use("/api/messages", messageRoutes);
 // Example root route
 // app.get("/", (req, res) => {
 //   res.send("App is ready");

3b8cd461beb1da7896bce24dd7b770a03e0124a1
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-09-23 12:54:53 +0530

    Added send message functionality and search functionality

--- frontend/chat-application/src/components/messages/MessageContainer.jsx
+++ frontend/chat-application/src/components/messages/MessageContainer.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import NoChatSelected from "./NoChatSelected";
+import useConversation from "../../store/useConversation";
+import { useEffect } from "react";
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    return () => {
+      //Clean up function
+      setSelectedConversation(null);
+    };
+  }, [setSelectedConversation]);
 
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To:</span>{" "}
-            <span className="text-gray-900 font-bold">John Doe</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation?.fullName}
+            </span>
           </div>
           <Messages />
           <MessageInput />

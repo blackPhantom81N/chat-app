161214395186f054c9a559bdf6b6c63d52921c5e
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-09-23 10:02:39 +0530

    Added Logout functionality with context

--- frontend/chat-application/src/pages/login/Login.jsx
+++ frontend/chat-application/src/pages/login/Login.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, loading } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -9,7 +19,7 @@ const Login = () => {
           <span className="text-blue-500">TalkSpace</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Username</span>
@@ -18,6 +28,8 @@ const Login = () => {
               type="text"
               placeholder="Enter Username"
               className="w-full input input-bordered h-10"
+              value={username}
+              onChange={(e) => setUsername(e?.target?.value)}
             />
             <div>
               <label className="label">
@@ -27,6 +39,8 @@ const Login = () => {
                 type="password"
                 placeholder="Enter Password"
                 className="w-full input input-bordered h-10"
+                value={password}
+                onChange={(e) => setPassword(e?.target?.value)}
               />
             </div>
             <Link
@@ -36,8 +50,15 @@ const Login = () => {
               {"Don't"} have an account?
             </Link>
             <div>
-              <button className="btn btn-block btn-sm mt-2 hover:bg-blue-600">
-                Login
+              <button
+                className="btn btn-block btn-sm mt-2 hover:bg-blue-600"
+                disabled={loading}
+              >
+                {loading ? (
+                  <span className="loading loading-spinner"></span>
+                ) : (
+                  "Login"
+                )}
               </button>
             </div>
           </div>
--- frontend/chat-application/src/pages/signup/Signup.jsx
+++ frontend/chat-application/src/pages/signup/Signup.jsx
@@ -102,8 +102,15 @@ const Signup = () => {
             Already have an account?
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 hover:bg-blue-600">
-              Login
+            <button
+              disabled={loading}
+              className="btn btn-block btn-sm mt-2 hover:bg-blue-600"
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>

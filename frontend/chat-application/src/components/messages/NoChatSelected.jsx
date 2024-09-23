ca452d0f6385ee71cfaa98c639ba7052d1e555a8
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-09-23 14:30:08 +0530

    Added online/offline functionality with socket.io

--- frontend/chat-application/src/components/messages/NoChatSelected.jsx
+++ frontend/chat-application/src/components/messages/NoChatSelected.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { LuMessagesSquare } from "react-icons/lu";
+import { useAuthContext } from "../../context/AuthContext";
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p> Welcome 👋 John Doe!</p>
+        <p> Welcome 👋 {authUser.fullName}! </p>
         <p>Select a chat to start messaging</p>
         <LuMessagesSquare className="text-3xl md:text-6xl text-center" />
       </div>

ba2d2ac8cc2d3fc50091e3c5e2b12eb55918eea0
Nikesha Silva <nikesha.silva@innovative-e.com>
2024-09-25 14:08:38 +0530

    Search input issue fixed

--- frontend/chat-application/src/components/sidebar/SearchInput.jsx
+++ frontend/chat-application/src/components/sidebar/SearchInput.jsx
@@ -5,7 +5,7 @@ import useGetConversations from "../../hooks/useGetConversations";
 import toast from "react-hot-toast";
 
 const SearchInput = () => {
-  const [search, setSearch] = useState(false);
+  const [search, setSearch] = useState("");
   const { setSelectedConversation } = useConversation();
   const { conversations } = useGetConversations();
 
